c2a51dfce899db7bb3aa76b832ddf308b23b8bb6
Jonathan Wexler <jwexman@gmail.com>
2016-04-19 19:26:50 -0400

    added locations model closes #6

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419230133) do
+ActiveRecord::Schema.define(version: 20160419232540) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.float    "lat"
+    t.float    "lon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "places", force: :cascade do |t|
