2c736a440cb4ec90421d437a18ab16e7c61960e1
Richard Mable <richardbmable@gmail.com>
2016-04-19 19:12:09 -0400

    quick fix for user model adding relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+	has_many :places
+	has_many :stories
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
