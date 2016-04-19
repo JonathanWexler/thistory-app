5045c32cddc0f4f6b05892c27146ee2bcc76724e
Zach Krzyzanowski <zkrzyz@gmail.com>
2016-04-19 19:45:25 -0400

    Added Place validations and controller

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
   has_and_belongs_to_many :stories
   has_one :location
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :name, presence: true
 end
