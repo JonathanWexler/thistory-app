7ecdf9b8d0d1a2de25150026c8b68ca023942810
Kyle N. Smith <smith.kylen@gmail.com>
2016-04-19 20:20:36 -0400

    added controller validations

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,18 @@
+class LocationsController < ApplicationController
+	def edit
+	end
+
+	def index
+	end
+	
+	def new
+	end
+
+	def destroy
+	end
+
+	def show
+	end
+	
+
+end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,10 @@
 class Story < ActiveRecord::Base
 	has_many :references
-	belongs_to :user
-	belongs_to :master_story
+	has_one :user
+	has_one :master_story
 	has_many :places
+
+	validates :user_id, presence: true:
+	validates :place_id, presence: true:
+	validates :title, presence: true:
 end
